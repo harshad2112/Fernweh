8c0c7df634d08ec43fd682354d4f62f258984f97
harshad <harshadaggarwal1999@gmail.com>
2021-08-05 11:30:17 +0530

    ui update

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports={
-    MONGODB:"mongodb+srv://harshad2112:9JnX4Gwka051R3SG@cluster0.izrjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MONGODB:"mongodb+srv://harshad2112:lEo2nRSdSG7Nwwjb@cluster0.izrjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
+// mongodb+srv://harshad2112:<password>@cluster0.izrjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// lEo2nRSdSG7Nwwjb
